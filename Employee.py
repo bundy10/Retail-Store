b20785c88de2b33fab2aff5ea71afe67d6b5a8de
bundy10 <bundyhuang1999@gmail.com>
2021-11-21 13:30:35 +1100

    Initial commit

---
+++ Employee.py
@@ -0,0 +1,7 @@
+class Employee: 
+    def __init__(self, fName, lName, age, DOB, experience):
+        self.fName = fName
+        self.lName = lName
+        self.age = age
+        self.DOB = DOB
+        self.experience = experience
